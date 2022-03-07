1253307479abf593125ff3cce76a70bf0849feaa
Veronika <veronika.pilipenko@gmail.com>
2022-03-07 17:23:53 -0500

    toggle button works in the console

--- toggle/src/App.js
+++ toggle/src/App.js
@@ -2,13 +2,18 @@ import { useState } from 'react';
 
 const App = () => {
 
-  const [light, setLight] = useState(false);
+  const [light, setLight] = useState(true);
 
   console.log({ light })
 
   return (
     <div className="h-screen bg-slate-100">
-      <button className="border-2 border-neutral-300 px-2 py-5">Toggle Light Switch</button>
+      <button 
+        className="border-2 border-neutral-300 px-2 py-5"
+        onClick={() => setLight(!light)}
+        >
+         Toggle Light Switch
+        </button>
     </div>
   );
 }
