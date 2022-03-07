506a58f6af5d9eeef79c1c71b5dc48080d4b9d87
Veronika <veronika.pilipenko@gmail.com>
2022-03-07 17:20:27 -0500

    added hooks

--- toggle/src/App.js
+++ toggle/src/App.js
@@ -1,4 +1,11 @@
+import { useState } from 'react';
+
 const App = () => {
+
+  const [light, setLight] = useState(false);
+
+  console.log({ light })
+
   return (
     <div className="h-screen bg-slate-100">
       <button className="border-2 border-neutral-300 px-2 py-5">Toggle Light Switch</button>
